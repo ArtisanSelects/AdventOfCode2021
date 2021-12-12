7889af85a86c4870956d33253be96dcb2ebbc866
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2021-12-12 15:22:15 -0500

    Day twelve updated to use sets for checking if a cave is small.

--- Day12/day12.py
+++ Day12/day12.py
@@ -4,32 +4,25 @@ from collections import defaultdict
 
 def solve_puzzle(puzzle_input):
 
-    def is_small_cave(cave):
-        return ord(cave[0]) >= 97
-
-    def valid_path_part_one(path, cave):
-        if is_small_cave(cave) and cave in path:
-            return False
-        return True
-
-    def valid_path_part_two(path, cave):
-        if is_small_cave(cave) and cave in path:
-            small_caves = [i for i in path if is_small_cave(i)]
-            if len(small_caves) != len(set(small_caves)):
+    def valid_path(path, cave, part_one=False):
+        if cave in small_caves and cave in path:
+            if part_one:
+                return False
+            path_caves = [i for i in path if i in small_caves]
+            if len(path_caves) != len(set(path_caves)):
                 return False
         return True
 
     def find_paths(part_one=False):
         res = 0
         to_check = [['start']]
-        processor = valid_path_part_one if part_one else valid_path_part_two
         while len(to_check) > 0:
             path = to_check.pop()
             connections = caves[path[-1]]
             for i in connections:
                 if i == 'end':
                     res += 1
-                elif processor(path, i):
+                elif valid_path(path, i, part_one):
                     to_check.append(path+[i])
         return res
 
@@ -39,9 +32,12 @@ def solve_puzzle(puzzle_input):
         caves[i[1]].append(i[0])
     for k, v in caves.items():
         caves[k] = [i for i in v if i != 'start']
+    small_caves = set([i for i in caves.keys() if
+                       ord(i[0]) >= 97 and i not in {'start', 'end'}])
     part_one = find_paths(True)
     part_two = find_paths()
     return part_one, part_two
+
 
 if __name__ == '__main__':
     puzzle_input = [i.split('-') for i in
